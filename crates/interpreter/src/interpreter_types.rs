d31ab90bb0f547f7954d01f323bf335015f5631a
Dmitry Savonin <nwtsoas@gmail.com>
2025-06-28 19:15:36 +0300

    chore(metadata): tiny refactoring

--- crates/interpreter/src/interpreter_types.rs
+++ crates/interpreter/src/interpreter_types.rs
@@ -42,10 +42,8 @@ pub trait InputsTr {
     fn input(&self) -> &CallInput;
     /// Returns call value of the call.
     fn call_value(&self) -> U256;
-
-    fn account_owner_address(&self) -> Option<Address> {
-        None
-    }
+    /// An account owner
+    fn account_owner_address(&self) -> Option<Address>;
 }
 
 /// Trait needed for legacy bytecode.
