338689273351ae4a71992196831f7e4dc6edf262
Dmitry Savonin <github@dmitry123.xyz>
2026-05-13 20:51:27 +0400

    fix: add missing type annotation

--- crates/interpreter/src/instructions/system.rs
+++ crates/interpreter/src/instructions/system.rs
@@ -28,7 +28,7 @@ pub fn keccak256<WIRE: InterpreterTypes, H: Host + ?Sized>(
     } else {
         let from = as_usize_or_fail!(context.interpreter, offset);
         resize_memory!(context.interpreter, context.host.gas_params(), from, len);
-        primitives::keccak256(context.interpreter.memory.slice_len(from, len).as_ref())
+        primitives::keccak256::<&[u8]>(context.interpreter.memory.slice_len(from, len).as_ref())
     };
     *top = hash.into();
 }
